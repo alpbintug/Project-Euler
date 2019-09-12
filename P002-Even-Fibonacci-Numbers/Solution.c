2d10f577b63de93897aec44699e221778434c194
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-12 18:56:06 +0300

    Update Solution.c

--- P002-Even-Fibonacci-Numbers/Solution.c
+++ P002-Even-Fibonacci-Numbers/Solution.c
@@ -4,12 +4,12 @@ int main(){
     int previous = 0;
     int next = 2;
     int sumOfEvenNumbers = 0;
-
+    int dummy;
     while(next<4000000){
-        sumOfEvenNumbers += next;
-        int dummy = next;
-        next = 4*next + previous;
-        previous = dummy;
+        sumOfEvenNumbers += next; //Adding the current even fibonacci value to the our sum
+        dummy = next; //Storing current value, because we will change this value.
+        next = 4*next + previous; //Calculating the next value
+        previous = dummy; //Updating the previous value.
     }
-    printf("%d",sumOfEvenNumbers);
+    printf("%d",sumOfEvenNumbers); //Output: 4613732
 }
