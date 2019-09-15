4e847bd040e1b08a239a0b592976d67ed24958ec
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-15 20:07:51 +0300

    Update Solution.c

--- P003-Largest-Prime-Number/Solution.c
+++ P003-Largest-Prime-Number/Solution.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    unsigned long long number = 600851475143LLU;
+    unsigned long long number = 600851475143LLU; // C Cannot store value using "int" because an integer has 4 bytes of space
+    //Number 600851475143 is hold 10 bytes of space in RAM.
+    //Hardest part of this problem was solving the storage issue of the number.
     int i;
 
     for(i = 2; i <= number; i++){
